5cb5a789c5fb59165d00da578552eca2c12c3c6a
Lianhao Lu <lianhao.lu@intel.com>
2010-10-13 10:58:01 +0800

    Added missing files into git track of commit 798d252.
    Signed-off-by: Lianhao Lu <lianhao.lu@intel.com>

---
+++ plugins/org.yocto.sdk.remotetools/src/org/yocto/sdk/remotetools/LocalJob.java
@@ -0,0 +1,74 @@
+/*******************************************************************************
+ * Copyright (c) 2010 Intel Corporation.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ * Intel - initial API and implementation
+ *******************************************************************************/
+
+package org.yocto.sdk.remotetools;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
+import org.eclipse.core.runtime.jobs.Job;
+
+public class LocalJob extends Job {
+	
+	public static final String LOCAL_JOB_FAMILY = "localJobFamily";
+	private String[] cmdarray;
+	private String[] envp;
+	private File dir;
+	
+	public LocalJob(String name, String[] cmdarray, String[] envp, File dir) {
+		super(name);
+		this.cmdarray=cmdarray;
+		this.envp=envp;
+		this.dir=dir;
+	}
+
+	@Override
+	protected IStatus run(IProgressMonitor monitor) {
+		Process p=null;
+		boolean cancel=false;
+		
+		try {
+			//start process
+			p=Runtime.getRuntime().exec(cmdarray,envp,dir);
+			
+			//wait for completion
+			while (!cancel) {
+	
+				if(monitor.isCanceled()) 
+					cancel=true;
+				
+				try {
+					p.exitValue();
+					break;
+				}catch (IllegalThreadStateException e) {
+				}
+				
+				Thread.sleep(500);
+			}
+			
+		}catch (IOException e) {
+			
+		}catch (InterruptedException e){
+			cancel=true;
+		}finally {
+			if(p!=null)
+				p.destroy();
+		}
+		return (cancel!=true) ? Status.OK_STATUS : Status.CANCEL_STATUS;
+	}
+	
+	public boolean belongsTo(Object family) {
+         return family == LOCAL_JOB_FAMILY;
+    }
+}

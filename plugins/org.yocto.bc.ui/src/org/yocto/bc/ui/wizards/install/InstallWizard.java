e3819aad8eaf5bea974318b1e7cf4079c45fa54e
Jessica Zhang <jessica.zhang@intel.com>
2010-12-29 22:40:58 -0800

    Finish option page contents and performFinish all the way to project creation
    Signed-off-by: Jessica Zhang <jessica.zhang@intel.com>

--- plugins/org.yocto.bc.ui/src/org/yocto/bc/ui/wizards/install/InstallWizard.java
+++ plugins/org.yocto.bc.ui/src/org/yocto/bc/ui/wizards/install/InstallWizard.java
@@ -6,6 +6,7 @@ import java.io.FileDescriptor;
 import java.io.InputStream;
 import java.io.IOException;
 import java.io.OutputStream;
+import java.io.Writer;
 import java.util.ArrayList;
 import java.util.Hashtable;
 import java.util.Map;
@@ -43,6 +44,8 @@ import org.yocto.bc.ui.Activator;
 import org.yocto.bc.ui.model.ProjectInfo;
 import org.yocto.bc.ui.wizards.FiniteStateWizard;
 //import org.yocto.bc.ui.wizards.install.InstallJob.UICommandResponseHandler;
+//import org.yocto.bc.ui.wizards.install.BitbakePage.ConsoleWriter;
+import org.yocto.bc.ui.wizards.newproject.BBConfigurationInitializeOperation;
 import org.yocto.bc.ui.wizards.newproject.CreateBBCProjectOperation;
 
 /**
@@ -51,18 +54,22 @@ import org.yocto.bc.ui.wizards.newproject.CreateBBCProjectOperation;
  * @author kgilmer
  * 
  */
-public class InstallWizard extends FiniteStateWizard implements IWorkbenchWizard {
+public class InstallWizard extends FiniteStateWizard implements
+		IWorkbenchWizard {
 
 	static final String KEY_PINFO = "KEY_PINFO";
 	protected static final String OPTION_MAP = "OPTION_MAP";
 	protected static final String INSTALL_SCRIPT = "INSTALL_SCRIPT";
 	protected static final String INSTALL_DIRECTORY = "Install Directory";
 	protected static final String INIT_SCRIPT = "Init Script";
+
+	protected static final String PROJECT_NAME = "Project Name";
 	protected static final String DEFAULT_INIT_SCRIPT = "poky-init-build-env";
 	protected static final String DEFAULT_INSTALL_DIR = "~/yocto";
-	//protected static final String INSTALL_SCRIPT = "INSTALL_SCRIPT";
-	//protected static final String INSTALL_DIRECTORY = "Install Directory";
-	//protected static final String INSTALL_SCRIPT_FILE = "scripts/poky_install.sh";
+	// protected static final String INSTALL_SCRIPT = "INSTALL_SCRIPT";
+	// protected static final String INSTALL_DIRECTORY = "Install Directory";
+	// protected static final String INSTALL_SCRIPT_FILE =
+	// "scripts/poky_install.sh";
 	private Map model;
 	private MessageConsole myConsole;
 
@@ -70,81 +77,72 @@ public class InstallWizard extends FiniteStateWizard implements IWorkbenchWizard
 		this.model = new Hashtable();
 		model.put(INSTALL_DIRECTORY, DEFAULT_INSTALL_DIR);
 		model.put(INIT_SCRIPT, DEFAULT_INIT_SCRIPT);
-		//try {
-		//	model.put(INSTALL_SCRIPT, InstallScriptHelper.loadFile("scripts/poky_install.sh"));
-		//} catch (IOException e) {
-			// TODO Auto-generated catch block
-		//	e.printStackTrace();
-		//}
+		// try {
+		// model.put(INSTALL_SCRIPT,
+		// InstallScriptHelper.loadFile("scripts/poky_install.sh"));
+		// } catch (IOException e) {
+		// TODO Auto-generated catch block
+		// e.printStackTrace();
+		// }
 		setWindowTitle("Yocto BitBake Commander");
 		setNeedsProgressMonitor(false);
-		//setDefaultPageImageDescriptor(Activator.getImageDescriptor("icons/OE_logo_96.png"));
+		// setDefaultPageImageDescriptor(Activator.getImageDescriptor("icons/OE_logo_96.png"));
 		myConsole = findConsole("Yocto Console");
 		IWorkbench wb = PlatformUI.getWorkbench();
 		IWorkbenchWindow win = wb.getActiveWorkbenchWindow();
 		IWorkbenchPage page = win.getActivePage();
 		String id = IConsoleConstants.ID_CONSOLE_VIEW;
 		try {
-		IConsoleView view = (IConsoleView) page.showView(id);
-		view.display(myConsole);
+			IConsoleView view = (IConsoleView) page.showView(id);
+			view.display(myConsole);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
 
-    private MessageConsole findConsole(String name) {
-    	ConsolePlugin plugin = ConsolePlugin.getDefault();
-    	IConsoleManager conMan = plugin.getConsoleManager();
-    	IConsole[] existing = conMan.getConsoles();
-    	for (int i = 0; i < existing.length; i++)
-    		if (name.equals(existing[i].getName()))
-    			return (MessageConsole) existing[i];
-    	//no console found, so create a new one
-    	MessageConsole myConsole = new MessageConsole(name, null);
-    	conMan.addConsoles(new IConsole[]{myConsole});
-    	return myConsole;
-    }
+	private MessageConsole findConsole(String name) {
+		ConsolePlugin plugin = ConsolePlugin.getDefault();
+		IConsoleManager conMan = plugin.getConsoleManager();
+		IConsole[] existing = conMan.getConsoles();
+		for (int i = 0; i < existing.length; i++)
+			if (name.equals(existing[i].getName()))
+				return (MessageConsole) existing[i];
+		// no console found, so create a new one
+		MessageConsole myConsole = new MessageConsole(name, null);
+		conMan.addConsoles(new IConsole[] { myConsole });
+		return myConsole;
+	}
 
 	public InstallWizard(IStructuredSelection selection) {
 		model = new Hashtable();
 	}
 
-	/*@Override
-	public IWizardPage getNextPage(IWizardPage page) {
-		if (page instanceof WelcomePage) {
-			if (model.containsKey(WelcomePage.ACTION_USE)) {
-				return bbcProjectPage;
-			}
-		} else if (page instanceof ProgressPage) {
-			return bitbakePage;
-		}
-		
-		if (super.getNextPage(page) != null) {
-			System.out.println("next page: " + super.getNextPage(page).getClass().getName());
-		} else {
-			System.out.println("end page");
-		}
-
-		return super.getNextPage(page);
-	}
-
-	@Override
-	public boolean canFinish() {
-		System.out.println("can finish: " + super.canFinish());
-		return super.canFinish();
-	}
-*/
+	/*
+	 * @Override public IWizardPage getNextPage(IWizardPage page) { if (page
+	 * instanceof WelcomePage) { if (model.containsKey(WelcomePage.ACTION_USE))
+	 * { return bbcProjectPage; } } else if (page instanceof ProgressPage) {
+	 * return bitbakePage; }
+	 * 
+	 * if (super.getNextPage(page) != null) { System.out.println("next page: " +
+	 * super.getNextPage(page).getClass().getName()); } else {
+	 * System.out.println("end page"); }
+	 * 
+	 * return super.getNextPage(page); }
+	 * 
+	 * @Override public boolean canFinish() { System.out.println("can finish: "
+	 * + super.canFinish()); return super.canFinish(); }
+	 */
 	@Override
 	public void addPages() {
-	//	flavorPage = new FlavorPage(model);
-	//	bitbakePage = new BitbakePage(model);
-	//	bbcProjectPage = new BBCProjectPage(model);
-		//addPage(new WelcomePage(model));
-		//JZaddPage(new FlavorPage(model));
+		// flavorPage = new FlavorPage(model);
+		// bitbakePage = new BitbakePage(model);
+		// bbcProjectPage = new BBCProjectPage(model);
+		// addPage(new WelcomePage(model));
+		// JZaddPage(new FlavorPage(model));
 		addPage(new OptionsPage(model));
-		//addPage(new ProgressPage(model));
-		//addPage(bbcProjectPage);
-		//addPage(new BitbakePage(model));
+		// addPage(new ProgressPage(model));
+		// addPage(bbcProjectPage);
+		// addPage(new BitbakePage(model));
 	}
 
 	@Override
@@ -154,69 +152,117 @@ public class InstallWizard extends FiniteStateWizard implements IWorkbenchWizard
 
 	@Override
 	public boolean performFinish() {
-		BCCommandResponseHandler cmdOut = new BCCommandResponseHandler(myConsole);
-		//Map options = (Map)model.get(OptionsPage.OPTION_MAP);
-		Map options = (Map)model;
-		String install_dir;
-		if (options.containsKey(INSTALL_DIRECTORY)) {
-			install_dir = (String)options.get(INSTALL_DIRECTORY);
-			System.out.println(install_dir);
-		}
-		
-		executeCommand(cmdOut);
+		BCCommandResponseHandler cmdOut = new BCCommandResponseHandler(
+				myConsole);
+		// Map options = (Map)model.get(OptionsPage.OPTION_MAP);
 		WizardPage page = (WizardPage) getPage("Options");
 		page.setPageComplete(true);
-		
-		if (!cmdOut.hasError()) {
-		ProjectInfo pinfo = (ProjectInfo) model.get(KEY_PINFO);
-		Activator.putProjInfo(pinfo.getRootPath(), pinfo);
-		try {
-			getContainer().run(false, false, new CreateBBCProjectOperation(pinfo));
-		} catch (Exception e) {
-			Activator.getDefault().getLog().log(new Status(IStatus.ERROR, Activator.PLUGIN_ID, IStatus.ERROR, e.getMessage(), e));
-			this.getContainer().getCurrentPage().setDescription("Failed to create project: " + e.getMessage());
-			return false;
+		Map options = (Map) model;
+		String install_dir = "";
+		if (options.containsKey(INSTALL_DIRECTORY)) {
+			install_dir = (String) options.get(INSTALL_DIRECTORY);
+			System.out.println(install_dir);
 		}
+		//String install_dir_cmd = "mkdir "+install_dir;
+		//cmdOut.printCmd(install_dir_cmd);
+		//executeCommand(cmdOut, install_dir_cmd);
+		//if (cmdOut.hasError())
+		//return true;
+
+		String git_clone_cmd = "git clone git://git.pokylinux.org/poky.git "
+				+ install_dir;
+		cmdOut.printCmd(git_clone_cmd);
+		executeCommand(cmdOut, git_clone_cmd);
+		System.out.println("after executeCommand");
+
+		if (!cmdOut.hasError()) {
+			
+			String initPath = install_dir + "/"
+					+ (String) options.get(INIT_SCRIPT);
+			String prjName = (String) options.get(PROJECT_NAME);
+			ProjectInfo pinfo = new ProjectInfo();
+			pinfo.setInitScriptPath(initPath);
+			pinfo.setLocation(install_dir);
+			pinfo.setName(prjName);
+
+			try {
+				ConsoleWriter cw = new ConsoleWriter();
+				this.getContainer().run(false, false,
+						new BBConfigurationInitializeOperation(pinfo, cw));
+				cmdOut.printCmd(cw.getContents());
+			} catch (Exception e) {
+				Activator
+						.getDefault()
+						.getLog()
+						.log(new Status(IStatus.ERROR, Activator.PLUGIN_ID,
+								IStatus.ERROR, e.getMessage(), e));
+				this.getContainer()
+						.getCurrentPage()
+						.setDescription(
+								"Failed to create project: " + e.getMessage());
+			}
+
+			model.put(InstallWizard.KEY_PINFO, pinfo);
+			// ProjectInfo pinfo = (ProjectInfo) model.get(KEY_PINFO);
+			Activator.putProjInfo(pinfo.getRootPath(), pinfo);
+			try {
+				getContainer().run(false, false,
+						new CreateBBCProjectOperation(pinfo));
+			} catch (Exception e) {
+				Activator
+						.getDefault()
+						.getLog()
+						.log(new Status(IStatus.ERROR, Activator.PLUGIN_ID,
+								IStatus.ERROR, e.getMessage(), e));
+				this.getContainer()
+						.getCurrentPage()
+						.setDescription(
+								"Failed to create project: " + e.getMessage());
+				return false;
+			}
 		}
 		return true;
 	}
 
-	private void executeCommand(BCCommandResponseHandler cmdOut) {
-		
-		try {
-			ShellSession shell = new ShellSession(ShellSession.SHELL_TYPE_BASH, null, null, null);
-			//Map options = (Map)model.get(OptionsPage.OPTION_MAP);
-			Map options = (Map)model;
-			//shell.execute("bash ~/workspace/plugins/org.yocto.bc.ui/scripts/poky_install.sh ~/poky-test", cmdOut);
-			shell.execute("bash /scripts/poky_install.sh ~/poky-test", cmdOut);
-			/*
-			while ((line = reader.readLine()) != null && !errorOccurred) {
-				line = line.trim();
-				if (line.length() > 0 && !line.startsWith("#")) {
-					line = substitute(line, vars);
-					cmdOut.printCmd(line);
-					shell.execute(line, cmdOut);
-				} else if (line.startsWith("#")) {
-					cmdOut.printDialog(line.substring(1).trim());
-				}
-			}
+	private void executeCommand(BCCommandResponseHandler cmdOut, String cmd) {
 
-			if (errorOccurred) {
-				return new Status(IStatus.ERROR, Activator.PLUGIN_ID, "Failed to install OpenEmbedded");
-			}*/
+		try {
+			ShellSession shell = new ShellSession(ShellSession.SHELL_TYPE_BASH,
+					null, null, null);
+
+			// Map options = (Map)model.get(OptionsPage.OPTION_MAP);
+			// Map options = (Map)model;
+
+			shell.execute(cmd, cmdOut);
+			// shell.execute("bash ~/workspace/plugins/org.yocto.bc.ui/scripts/poky_install.sh ~/poky-test",
+			// cmdOut);
+			// shell.execute("bash /scripts/poky_install.sh ~/poky-test",
+			// cmdOut);
+			/*
+			 * while ((line = reader.readLine()) != null && !errorOccurred) {
+			 * line = line.trim(); if (line.length() > 0 &&
+			 * !line.startsWith("#")) { line = substitute(line, vars);
+			 * cmdOut.printCmd(line); shell.execute(line, cmdOut); } else if
+			 * (line.startsWith("#")) {
+			 * cmdOut.printDialog(line.substring(1).trim()); } }
+			 * 
+			 * if (errorOccurred) { return new Status(IStatus.ERROR,
+			 * Activator.PLUGIN_ID, "Failed to install OpenEmbedded"); }
+			 */
 		} catch (IOException e) {
 			e.printStackTrace();
-			//return new Status(IStatus.ERROR, Activator.PLUGIN_ID, "Failed to install OpenEmbedded", e);
+			// return new Status(IStatus.ERROR, Activator.PLUGIN_ID,
+			// "Failed to install OpenEmbedded", e);
 		}
-		//return Status.OK_STATUS;
+		// return Status.OK_STATUS;
 	}
-		
+
 	public void init(IWorkbench workbench, IStructuredSelection selection) {
 	}
-	
+
 	private class BCCommandResponseHandler implements ICommandResponseHandler {
 		private MessageConsoleStream myConsoleStream;
-		private  Boolean errorOccured = false;
+		private Boolean errorOccured = false;
 
 		public BCCommandResponseHandler(MessageConsole console) {
 			try {
@@ -237,7 +283,7 @@ public class InstallWizard extends FiniteStateWizard implements IWorkbenchWizard
 		public Boolean hasError() {
 			return errorOccured;
 		}
-		
+
 		public void response(String line, boolean isError) {
 			try {
 				if (isError) {
@@ -259,4 +305,38 @@ public class InstallWizard extends FiniteStateWizard implements IWorkbenchWizard
 			}
 		}
 	}
+
+	private class ConsoleWriter extends Writer {
+
+		private StringBuffer sb;
+
+		public ConsoleWriter() {
+			sb = new StringBuffer();
+		}
+
+		@Override
+		public void close() throws IOException {
+		}
+
+		public String getContents() {
+			return sb.toString();
+		}
+
+		@Override
+		public void flush() throws IOException {
+		}
+
+		@Override
+		public void write(char[] cbuf, int off, int len) throws IOException {
+			// txtConsole.getText().concat(new String(cbuf));
+			sb.append(new String(cbuf));
+		}
+
+		@Override
+		public void write(String str) throws IOException {
+			sb.append(str);
+		}
+
+	}
+
 }
--- plugins/org.yocto.bc.ui/src/org/yocto/bc/ui/wizards/install/OptionsPage.java
+++ plugins/org.yocto.bc.ui/src/org/yocto/bc/ui/wizards/install/OptionsPage.java
@@ -7,6 +7,11 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IResource;
+import org.eclipse.core.resources.IWorkspaceRoot;
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.IStatus;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.SelectionAdapter;
 import org.eclipse.swt.events.SelectionEvent;
@@ -21,7 +26,10 @@ import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Text;
 import org.eclipse.ui.PlatformUI;
 
+import org.yocto.bc.ui.wizards.FiniteStateWizard;
 import org.yocto.bc.ui.wizards.FiniteStateWizardPage;
+import org.yocto.bc.ui.wizards.FiniteStateWizardPage.ValidationListener;
+//import org.yocto.bc.ui.wizards.install.BBCProjectPage.FileOpenSelectionAdapter;
 
 /**
  * Select which flavor of OE is to be installed.
@@ -37,6 +45,12 @@ public class OptionsPage extends FiniteStateWizardPage {
 	
 	private List controlList;
 	private boolean controlsCreated = false;
+	
+	private Text txtProjectLocation;
+
+	private Text txtInit;
+	private ValidationListener validationListener;
+	private Text txtProjectName;
 
 	protected OptionsPage(Map model) {
 		super("Options", model);
@@ -50,10 +64,82 @@ public class OptionsPage extends FiniteStateWizardPage {
 		top.setLayout(new GridLayout());
 		top.setLayoutData(new GridData(GridData.FILL_BOTH));
 
-		c1 = new Composite(top, SWT.None);
-		c1.setLayout(new GridLayout(2, false));
-		c1.setLayoutData(new GridData(GridData.FILL_BOTH));
+		//c1 = new Composite(top, SWT.None);
+		//c1.setLayout(new GridLayout(2, false));
+		//c1.setLayoutData(new GridData(GridData.FILL_BOTH));
 
+		
+		GridData gdFillH = new GridData(GridData.FILL_HORIZONTAL);
+		GridData gdVU = new GridData(GridData.VERTICAL_ALIGN_BEGINNING);
+		
+		//Composite top = new Composite(parent, SWT.NONE);
+		//top.setLayoutData(new GridData(GridData.FILL_BOTH));
+		//top.setLayout(new GridLayout());
+
+		Composite projectNameComp = new Composite(top, SWT.NONE);
+		GridData gdProjName = new GridData(GridData.FILL_HORIZONTAL);
+		projectNameComp.setLayoutData(gdProjName);
+		projectNameComp.setLayout(new GridLayout(2, false));
+		Label lblProjectName = new Label(projectNameComp, SWT.NONE);
+		lblProjectName.setText("Project N&ame:");
+
+		txtProjectName = new Text(projectNameComp, SWT.BORDER);
+		txtProjectName.setLayoutData(gdFillH);
+		txtProjectName.setFocus();
+		validationListener = new ValidationListener();
+		
+		txtProjectName.addModifyListener(validationListener);
+
+		Label lblProjectLocation = new Label(projectNameComp, SWT.None);
+		lblProjectLocation.setText("&Location:");
+
+		Composite locComposite = new Composite(projectNameComp, SWT.NONE);
+		GridData gd = new GridData(GridData.VERTICAL_ALIGN_END
+				| GridData.FILL_HORIZONTAL);
+		gd.horizontalIndent = 0;
+		locComposite.setLayoutData(gd);
+		GridLayout gl = new GridLayout(2, false);
+		gl.marginWidth = 0;
+		locComposite.setLayout(gl);
+
+		txtProjectLocation = new Text(locComposite, SWT.BORDER);
+		txtProjectLocation.setLayoutData(gdFillH);
+		txtProjectLocation.addModifyListener(validationListener);
+
+		Button button = new Button(locComposite, SWT.PUSH);
+		button.setText("Browse...");
+		button.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				handleBrowse();
+			}
+		});
+
+		Label lblInit = new Label(projectNameComp, SWT.NONE);
+		lblInit.setText("Init Script:");
+
+		Composite initComposite = new Composite(projectNameComp, SWT.NONE);
+		gd = new GridData(GridData.VERTICAL_ALIGN_END
+				| GridData.FILL_HORIZONTAL);
+		gd.horizontalIndent = 0;
+		initComposite.setLayoutData(gd);
+		gl = new GridLayout(2, false);
+		gl.marginWidth = 0;
+		initComposite.setLayout(gl);
+
+		txtInit = new Text(initComposite, SWT.BORDER);
+		GridData gdi = new GridData(GridData.FILL_HORIZONTAL);
+		txtInit.setLayoutData(gdi);
+		txtInit.addModifyListener(validationListener);
+
+		//Button btnLoadInit = new Button(initComposite, SWT.PUSH);
+		//btnLoadInit.setLayoutData(gdVU);
+		//btnLoadInit.setText("Choose...");
+		//btnLoadInit.addSelectionListener(new FileOpenSelectionAdapter());
+
+		//if (System.getenv("OEROOT") != null) {
+		//	txtProjectLocation.setText(System.getenv("OEROOT"));
+		//}
 		setControl(top);
 	}
 
@@ -106,6 +192,7 @@ public class OptionsPage extends FiniteStateWizardPage {
 		field.setText((String)model.get(InstallWizard.INIT_SCRIPT));
 		field.addModifyListener(listener);
 		cl.add(field);
+		
 		
 /*
 		for (Iterator i = v.keySet().iterator(); i.hasNext();) {
@@ -203,6 +290,14 @@ public class OptionsPage extends FiniteStateWizardPage {
 		*/
 	}
 
+	private void handleBrowse() {
+		DirectoryDialog dialog = new DirectoryDialog(getShell(), SWT.None);
+		String dir = dialog.open();
+		if (dir != null) {
+			txtProjectLocation.setText(dir);
+		}
+	}
+	
 	@Override
 	public void pageCleanup() {
 
@@ -210,6 +305,7 @@ public class OptionsPage extends FiniteStateWizardPage {
 
 	@Override
 	public void pageDisplay() {
+		/*
 		if (!controlsCreated) {
 			controlList = new ArrayList();
 			//model.put(INSTALL_SCRIPT, InstallScriptHelper.loadFile(f.getScriptURL()));
@@ -228,7 +324,7 @@ public class OptionsPage extends FiniteStateWizardPage {
 				// TODO Auto-generated catch block
 			//	e.printStackTrace();
 			//}
-		}
+		}*/
 	}
 
 	public static Map parseVars(String line) {
@@ -257,8 +353,12 @@ public class OptionsPage extends FiniteStateWizardPage {
 	@Override
 	
 	protected void updateModel() {
-		model.put(InstallWizard.INSTALL_DIRECTORY,((Text)controlList.get(0)).getText());
-		model.put(InstallWizard.INIT_SCRIPT,((Text)controlList.get(1)).getText());
+		//model.put(InstallWizard.INSTALL_DIRECTORY,((Text)controlList.get(0)).getText());
+		//model.put(InstallWizard.INIT_SCRIPT,((Text)controlList.get(1)).getText());
+		model.put(InstallWizard.INIT_SCRIPT, txtInit.getText());
+		model.put(InstallWizard.INSTALL_DIRECTORY, txtProjectLocation.getText());
+		model.put(InstallWizard.PROJECT_NAME, txtProjectName.getText());
+		
 		/*
 		controlList.get(0;)
 		Map m = new Hashtable();
@@ -279,9 +379,52 @@ public class OptionsPage extends FiniteStateWizardPage {
 		model.put(OPTION_MAP, m);*/
 	}
 
+	private boolean isValidProjectName(String projectName) {
+		if (projectName.indexOf('$') > -1) {
+			return false;
+		}
+
+		return true;
+	}
 	@Override
 	protected boolean validatePage() {
+		IWorkspaceRoot wsroot = ResourcesPlugin.getWorkspace().getRoot();
+
+		IStatus validate = ResourcesPlugin.getWorkspace().validateName(txtProjectName.getText(), IResource.PROJECT);
+
+		if (!validate.isOK() || !isValidProjectName(txtProjectName.getText())) {
+			setErrorMessage("Invalid project name: " + txtProjectName.getText());
+			return false;
+		}
+
+		IProject proj = wsroot.getProject(txtProjectName.getText());
+		if (proj.exists()) {
+			setErrorMessage("A project with the name " + txtProjectName.getText()
+					+ " already exists");
+			return false;
+		}
+		updateModel();
+		setPageComplete(true);
+		((FiniteStateWizard)this.getWizard()).setCanFinish(true);
 		return true;
+	}
+	
+	private class FileOpenSelectionAdapter extends SelectionAdapter {
+		@Override
+		public void widgetSelected(SelectionEvent e) {
+			FileDialog fd = new FileDialog(PlatformUI.getWorkbench()
+					.getDisplay().getActiveShell(), SWT.OPEN);
+
+			fd.setText("Open Configuration Script");
+			fd.setFilterPath(txtProjectLocation.getText());
+
+			String selected = fd.open();
+
+			if (selected != null) {
+				txtInit.setText(selected);
+				updateModel();
+			}
+		}
 	}
 
 }

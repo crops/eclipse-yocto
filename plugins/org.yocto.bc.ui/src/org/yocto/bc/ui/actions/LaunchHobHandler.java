52d663165661249cc0ca1a4dac52c98781039f80
Jessica Zhang <jessica.zhang@intel.com>
2011-06-23 15:15:24 -0700

    remove override to fix build issues
    Signed-off-by: Jessica Zhang <jessica.zhang@intel.com>

--- plugins/org.yocto.bc.ui/src/org/yocto/bc/ui/actions/LaunchHobHandler.java
+++ plugins/org.yocto.bc.ui/src/org/yocto/bc/ui/actions/LaunchHobHandler.java
@@ -27,31 +27,26 @@ public class LaunchHobHandler implements IHandler {
 		return null;
 	}
 
-	@Override
 	public void addHandlerListener(IHandlerListener handlerListener) {
 		// TODO Auto-generated method stub
 		
 	}
 
-	@Override
 	public void dispose() {
 		// TODO Auto-generated method stub
 		
 	}
 
-	@Override
 	public boolean isEnabled() {
 		// TODO Auto-generated method stub
 		return true;
 	}
 
-	@Override
 	public boolean isHandled() {
 		// TODO Auto-generated method stub
 		return true;
 	}
 
-	@Override
 	public void removeHandlerListener(IHandlerListener handlerListener) {
 		// TODO Auto-generated method stub
 		

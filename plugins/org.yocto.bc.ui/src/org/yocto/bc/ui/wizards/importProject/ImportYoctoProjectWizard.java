aabc30d29e1cc5fa86add34a35083b845ef6d40a
Jessica Zhang <jessica.zhang@intel.com>
2011-09-21 14:46:10 -0700

    Fix Copyright header
    Signed-off-by: Jessica Zhang <jessica.zhang@intel.com>

--- plugins/org.yocto.bc.ui/src/org/yocto/bc/ui/wizards/importProject/ImportYoctoProjectWizard.java
+++ plugins/org.yocto.bc.ui/src/org/yocto/bc/ui/wizards/importProject/ImportYoctoProjectWizard.java
@@ -1,3 +1,13 @@
+/*******************************************************************************
+ * Copyright (c) 2011 Intel Corporation.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ * Intel - initial API and implementation
+ *******************************************************************************/
 package org.yocto.bc.ui.wizards.importProject;
 
 import java.io.IOException;

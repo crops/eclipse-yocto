514c7a653b35a86ea30f0670831e4558e39b5907
Lianhao Lu <lianhao.lu@intel.com>
2012-01-29 16:23:27 +0800

    ide: Changed due to upstream changing in Autotool.
    [YOCTO #1915]
    
    Signed-off-by: Lianhao Lu <lianhao.lu@intel.com>

--- plugins/org.yocto.sdk.ide/src/org/yocto/sdk/ide/actions/ReconfigYoctoHandler.java
+++ plugins/org.yocto.sdk.ide/src/org/yocto/sdk/ide/actions/ReconfigYoctoHandler.java
@@ -12,23 +12,12 @@ package org.yocto.sdk.ide.actions;
 
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
-import org.eclipse.core.resources.IContainer;
-import org.eclipse.core.expressions.IEvaluationContext;
 import org.eclipse.linuxtools.internal.cdt.autotools.ui.actions.AbstractAutotoolsHandler;
 
 @SuppressWarnings("restriction")
 public class ReconfigYoctoHandler extends AbstractAutotoolsHandler {
 
 	public Object execute(ExecutionEvent event) throws ExecutionException {
-		ReconfigYoctoAction a = new ReconfigYoctoAction();
-		Object o = event.getApplicationContext();
-		if (o instanceof IEvaluationContext) {
-			IContainer container = getContainer((IEvaluationContext)o);
-			if (container != null) {
-				a.setSelectedContainer(container);
-				a.run(null);
-			}
-		}
-		return null;
+		return execute(event, new ReconfigYoctoAction());	
 	}
 }

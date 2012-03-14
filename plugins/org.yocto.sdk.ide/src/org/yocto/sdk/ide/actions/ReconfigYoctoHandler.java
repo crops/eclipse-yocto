1eef5ba930971ca07737e779cb4d0dd5ea0dcf04
Lianhao Lu <lianhao.lu@intel.com>
2012-03-14 17:10:46 +0800

    sdk.ide: Detect autotools.ui plugin version in runtime
    [Yocto #1915].
    Detect whether the method of AbstractAutotoolsHandler.execute() exists in
    runtime. This is a workaround in case the upstream eclipse bug #370288
    is not got fixed in time in Yocto 1.2 release cycle.
    
    Signed-off-by: Lianhao Lu <lianhao.lu@intel.com>

--- plugins/org.yocto.sdk.ide/src/org/yocto/sdk/ide/actions/ReconfigYoctoHandler.java
+++ plugins/org.yocto.sdk.ide/src/org/yocto/sdk/ide/actions/ReconfigYoctoHandler.java
@@ -10,14 +10,69 @@
  *******************************************************************************/
 package org.yocto.sdk.ide.actions;
 
+import java.lang.reflect.Method;
+
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.core.expressions.IEvaluationContext;
+import org.eclipse.core.resources.IContainer;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
+import org.eclipse.jface.dialogs.ErrorDialog;
 import org.eclipse.linuxtools.internal.cdt.autotools.ui.actions.AbstractAutotoolsHandler;
+import org.eclipse.linuxtools.internal.cdt.autotools.ui.actions.InvokeAction;
+import org.eclipse.swt.widgets.Display;
+import org.yocto.sdk.ide.YoctoSDKPlugin;
+
 
 @SuppressWarnings("restriction")
 public class ReconfigYoctoHandler extends AbstractAutotoolsHandler {
 
 	public Object execute(ExecutionEvent event) throws ExecutionException {
-		return execute(event, new ReconfigYoctoAction());	
+		ReconfigYoctoAction action = new ReconfigYoctoAction();
+		Method method = null;
+
+		try {
+			/*
+			 * This is hack to workaround upstream eclipse bug #370288
+			 */
+			Class [] params = {ExecutionEvent.class, InvokeAction.class};
+			method = AbstractAutotoolsHandler.class.getDeclaredMethod("execute",params );
+		} catch (NoSuchMethodException e) {
+			//no such method, old version of plugin org.eclipse.linuxtools.autotools.ui
+			method = null;
+		} catch (Exception e) {
+			throw new ExecutionException(e.getMessage(), e);
+		}
+
+		if (method != null) {
+			//new version
+			Object [] params = {event, action};
+			try {
+				return method.invoke(this, params);
+			}catch (Exception e) {
+				throw new ExecutionException(e.getMessage(), e);
+			}
+		} else {
+			//old version
+			//display a dialog to warn the user
+			Display.getDefault().syncExec(new Runnable() {
+				public void run() {
+					ErrorDialog.openError(null, "Change Yocto Project Settings", "Please update the plugin of \"Autotools support for CDT\"!", 
+							new Status(IStatus.WARNING,YoctoSDKPlugin.PLUGIN_ID,"old version of plugin \"Autotools support for CDT\" detected."));
+				}
+			});
+			//try to display the dialog in the old way
+			Object o = event.getApplicationContext();
+			if (o instanceof IEvaluationContext) {
+				IContainer container = getContainer((IEvaluationContext)o);
+				if (container != null) {
+					action.setSelectedContainer(container);
+					action.run(null);
+				}
+			}
+		}
+		
+		return null;
 	}
 }

5432591cf6f0c9eb002bc3abca3e32cb2feed976
Atanas Gegov <atanas.gegov@bmw-carit.de>
2013-05-23 10:39:50 +0200

    plugins/cmake: Added CMake template projects
    Added a C and C++ HelloWorld template projects for
    CMake. The both have the new CMake project type
    and are therefore built with the CMake toolchain.

---
+++ plugins/org.yocto.cmake.managedbuilder/templates/projecttemplates/HelloWorldCCMakeProject/src/main.c
@@ -0,0 +1,21 @@
+/** @mainpage $(projectName) - $(vendor)
+ *
+ * @author $(author) <$(email)>
+ * @version $(projectVersion)
+**/
+
+
+#include <stdio.h>
+/**
+ * Main class of project $(projectName)
+ *
+ * @param argc the number of arguments
+ * @param argv the arguments from the commandline
+ * @returns exit code of the application
+ */
+int main(int argc, char **argv) {
+	// print a greeting to the console
+	printf("Hello World!\n");
+
+	return 0;
+}

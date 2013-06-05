c6f89cee9ef1ada7323c3c3222d52166078740db
Ioana Grigoropol <ioanax.grigoropol@intel.com>
2013-06-05 19:44:18 +0300

    Retrieve console from RemoteHelper instead of recreating it
    Signed-off-by: Ioana Grigoropol <ioanax.grigoropol@intel.com>

--- plugins/org.yocto.bc.ui/src/org/yocto/bc/ui/wizards/install/InstallWizard.java
+++ plugins/org.yocto.bc.ui/src/org/yocto/bc/ui/wizards/install/InstallWizard.java
@@ -96,32 +96,9 @@ public class InstallWizard extends FiniteStateWizard implements
 		
 		setWindowTitle("Yocto Project BitBake Commander");
 		setNeedsProgressMonitor(true);
-		
-		myConsole = findConsole("Yocto Project Console");
-		IWorkbench wb = PlatformUI.getWorkbench();
-		IWorkbenchWindow win = wb.getActiveWorkbenchWindow();
-		IWorkbenchPage page = win.getActivePage();
-		String id = IConsoleConstants.ID_CONSOLE_VIEW;
-		try {
-			IConsoleView view = (IConsoleView) page.showView(id);
-			view.display(myConsole);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
 	}
 
-	private MessageConsole findConsole(String name) {
-		ConsolePlugin plugin = ConsolePlugin.getDefault();
-		IConsoleManager conMan = plugin.getConsoleManager();
-		IConsole[] existing = conMan.getConsoles();
-		for (int i = 0; i < existing.length; i++)
-			if (name.equals(existing[i].getName()))
-				return (MessageConsole) existing[i];
-		// no console found, so create a new one
-		MessageConsole myConsole = new MessageConsole(name, null);
-		conMan.addConsoles(new IConsole[] { myConsole });
-		return myConsole;
-	}
+
 
 	public InstallWizard(IStructuredSelection selection) {
 		model = new Hashtable();

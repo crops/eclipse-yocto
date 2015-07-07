05ba7b6791083a70c78366f9737e27d9dd25d08b
Todor Minchev <todor.minchev@intel.com>
2015-07-07 13:13:22 -0700

    org.yocto.bc: remove dependency on org.yocto.sdk.ide

--- plugins/org.yocto.remote.utils/src/org/yocto/remote/utils/Activator.java
+++ plugins/org.yocto.remote.utils/src/org/yocto/remote/utils/Activator.java
@@ -10,12 +10,8 @@
  *******************************************************************************/
 package org.yocto.remote.utils;
 
-import java.util.logging.Logger;
-
 import org.eclipse.ui.plugin.AbstractUIPlugin;
 import org.osgi.framework.BundleContext;
-import org.yocto.sdk.ide.preferences.LoggerConstants;
-import org.yocto.sdk.ide.utils.YoctoSDKUtils;
 
 /**
  * The activator class controls the plug-in life cycle
@@ -27,7 +23,6 @@ public class Activator extends AbstractUIPlugin {
 
 	// The shared instance
 	private static Activator plugin;
-	public static Logger logger ;
 
 	/**
 	 * The constructor
@@ -43,8 +38,6 @@ public class Activator extends AbstractUIPlugin {
 	public void start(BundleContext context) throws Exception {
 		super.start(context);
 		plugin = this;
-		logger = YoctoSDKUtils.registerLogger(LoggerConstants.RU_LOGGER_NAME, LoggerConstants.RU_LOG_FILE) ;
-
 	}
 
 	/*
@@ -55,7 +48,6 @@ public class Activator extends AbstractUIPlugin {
 	public void stop(BundleContext context) throws Exception {
 		plugin = null;
 		super.stop(context);
-		YoctoSDKUtils.unRegisterLogger(logger, LoggerConstants.RU_LOG_FILE) ;
 	}
 
 	/**
